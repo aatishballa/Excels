6d0e7fcf927451059666360b9654361a5c87e93e
Richard Case <cx3698ff@live.bemidjistate.edu>
2018-03-06 21:52:06 -0600

    graphs

--- The Graphinator/Form1.Designer.cs
+++ The Graphinator/Form1.Designer.cs
@@ -28,12 +28,159 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.T_Equation = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.T_Start = new System.Windows.Forms.TextBox();
+			this.T_End = new System.Windows.Forms.TextBox();
+			this.T_Interval = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 476);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(90, 24);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Equation:";
+			// 
+			// T_Equation
+			// 
+			this.T_Equation.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.T_Equation.Location = new System.Drawing.Point(108, 476);
+			this.T_Equation.Name = "T_Equation";
+			this.T_Equation.Size = new System.Drawing.Size(461, 26);
+			this.T_Equation.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(15, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(628, 455);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(574, 476);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(69, 26);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Graph";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.checkBox1.Location = new System.Drawing.Point(572, 519);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(71, 28);
+			this.checkBox1.TabIndex = 4;
+			this.checkBox1.Text = "Save";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(12, 519);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(51, 24);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Start:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(171, 519);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(50, 24);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "End:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(326, 519);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(74, 24);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Interval:";
+			// 
+			// T_Start
+			// 
+			this.T_Start.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.T_Start.Location = new System.Drawing.Point(69, 517);
+			this.T_Start.Name = "T_Start";
+			this.T_Start.Size = new System.Drawing.Size(85, 26);
+			this.T_Start.TabIndex = 8;
+			// 
+			// T_End
+			// 
+			this.T_End.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.T_End.Location = new System.Drawing.Point(227, 517);
+			this.T_End.Name = "T_End";
+			this.T_End.Size = new System.Drawing.Size(85, 26);
+			this.T_End.TabIndex = 9;
+			// 
+			// T_Interval
+			// 
+			this.T_Interval.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.T_Interval.Location = new System.Drawing.Point(401, 517);
+			this.T_Interval.Name = "T_Interval";
+			this.T_Interval.Size = new System.Drawing.Size(85, 26);
+			this.T_Interval.TabIndex = 10;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(655, 559);
+			this.Controls.Add(this.T_Interval);
+			this.Controls.Add(this.T_End);
+			this.Controls.Add(this.T_Start);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.T_Equation);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "The Graphinator";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox T_Equation;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox T_Start;
+		private System.Windows.Forms.TextBox T_End;
+		private System.Windows.Forms.TextBox T_Interval;
 	}
 }
 
--- The Graphinator/Form1.cs
+++ The Graphinator/Form1.cs
@@ -1,20 +1,153 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace The_Graphinator
 {
 	public partial class Form1 : Form
 	{
+		private Boolean saveXL;
+		private double start, end, interval;
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void loadSteps() {
+			try
+			{
+				interval = double.Parse(T_Interval.Text);
+			}
+			catch (Exception ex)
+			{
+				interval = 0.1;
+			}
+
+			try
+			{
+				start = double.Parse(T_Start.Text);
+			}
+			catch (Exception ex)
+			{
+				start = 0;
+			}
+
+			try
+			{
+				end = double.Parse(T_End.Text);
+			}
+			catch (Exception ex)
+			{
+				end = 10;
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			loadSteps();
+			Excel.Application xlApp;
+			Excel.Workbook xlWorkBook;
+			Excel.Worksheet xlWorkSheet;
+			object misValue = System.Reflection.Missing.Value;
+
+			xlApp = new Excel.Application();
+			xlApp.DisplayAlerts = false;
+
+			xlWorkBook = xlApp.Workbooks.Add(misValue);
+			xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+			//add data 
+			int max = (int)((end - start) / interval);
+			for (int i = 1; i <= max; i++)
+			{
+				double x = i * interval;
+
+				string xlEquation = "=";
+
+				foreach (char c in T_Equation.Text)
+				{
+					if (c == 'X' || c == 'x')
+					{
+						xlEquation += x;
+					}
+					else
+					{
+						xlEquation += c;
+					}
+				}
+
+				xlWorkSheet.Cells[i, 1] = xlEquation;
+			}
+
+			Excel.Range chartRange;
+
+			Excel.ChartObjects xlCharts = (Excel.ChartObjects)xlWorkSheet.ChartObjects(Type.Missing);
+			Excel.ChartObject myChart = (Excel.ChartObject)xlCharts.Add(0, 0, 465, 330);
+			Excel.Chart chartPage = myChart.Chart;
+
+			chartRange = xlWorkSheet.get_Range("A1", "A" + max);
+			//chartRange = xlWorkSheet.get_Range("A1", "A100"); //0.1 increments up to 10
+			chartPage.SetSourceData(chartRange, misValue);
+			chartPage.ChartType = Excel.XlChartType.xlLine;
+			chartPage.Legend.Clear();
+			
+			Excel.Axis yaxis = (Excel.Axis)chartPage.Axes(Excel.XlAxisType.xlValue, Excel.XlAxisGroup.xlPrimary);
+			yaxis.HasTitle = true;
+			yaxis.AxisTitle.Text = "Y";
+
+			Excel.Axis xaxis = (Excel.Axis)chartPage.Axes(Excel.XlAxisType.xlCategory, Excel.XlAxisGroup.xlPrimary);
+			xaxis.HasTitle = true;
+			xaxis.AxisTitle.Text = "X";
+
+
+			//export chart as picture file
+			if (pictureBox1.Image != null)
+			{
+				pictureBox1.Image.Dispose();
+				pictureBox1.Image = null;
+			}
+			chartPage.Export(AppDomain.CurrentDomain.BaseDirectory + "test.bmp", "BMP", misValue);
+
+			//load picture to picturebox
+			pictureBox1.Image = new Bitmap(AppDomain.CurrentDomain.BaseDirectory + "test.bmp");
+
+			xlWorkBook.Close(saveXL, misValue, misValue);
+			xlApp.Quit();
+
+			releaseObject(xlWorkSheet);
+			releaseObject(xlWorkBook);
+			releaseObject(xlApp);
+			releaseObject(chartPage);
+		}
+
+		private void releaseObject(object obj)
+		{
+			try
+			{
+				System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+				obj = null;
+			}
+			catch (Exception ex)
+			{
+				obj = null;
+				MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+			}
+			finally
+			{
+				GC.Collect();
+			}
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			saveXL = !saveXL;
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
